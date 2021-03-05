68f2a7f5ee0119ad5e432a8cd786290cdd6501f7
GoodB0y08 <78499786+GoodB0y08@users.noreply.github.com>
2021-03-05 18:06:20 +0200

    Delete spamOpenDoor.lua

--- spamOpenDoor.lua
+++
@@ -1,8 +0,0 @@
-    while bool do
-        for _,v in pairs(workspace.FunctionalDoors:GetChildren()) do
-            spawn(function()
-                v.Config.DetectEvent:FireServer()
-            end)
-        end
-        wait()
-    end
